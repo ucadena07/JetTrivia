2102dc3351e176f1d0454924df0b6d389a735eef
Ulises Cadena <ucadena07@gmail.com>
2023-11-20 05:41:31 -0600

    creating the viewmodel

---
+++ app/src/main/java/com/example/jettrivia/screen/QuestionsViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.jettrivia.screen
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jettrivia.data.DataOrException
+import com.example.jettrivia.model.QuestionItem
+import com.example.jettrivia.repository.QuestionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionsViewModel @Inject constructor(private val repo: QuestionRepository) : ViewModel() {
+    private val data : MutableState<DataOrException<ArrayList<QuestionItem>,Boolean,Exception>>
+                = mutableStateOf(DataOrException(null,true,Exception("")))
+    init {
+        getAllQuestions()
+    }
+
+    private fun getAllQuestions(){
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repo.getAllQuestions()
+            if(data.value.data.toString().isNotEmpty()){
+                data.value.loading = false
+            }
+        }
+    }
+}

3a16a8815b3c1d9c6dd502cd9c048c46726b5468
Ulises Cadena <ucadena07@gmail.com>
2023-11-21 06:32:43 -0600

    creating ui - radio btn row

--- app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -3,19 +3,30 @@ package com.example.jettrivia.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextRange
@@ -29,31 +40,68 @@ import androidx.compose.ui.unit.sp
 import com.example.jettrivia.screen.QuestionsViewModel
 import com.example.jettrivia.util.AppColors
 import androidx.compose.ui.graphics.PathEffect
+import com.example.jettrivia.model.QuestionItem
 
 @Composable
 fun Questions(viewModel: QuestionsViewModel) {
     val questions = viewModel.data.value.data?.toMutableList()
-    if(viewModel.data.value.loading == true){
+    if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
-    }else{
-       QuestionDisplay()
+    } else {
+        QuestionDisplay()
     }
 
 
 }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionDisplay(){
-    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f), 0f)
-    Surface(modifier = Modifier
-        .fillMaxSize()
-        .padding(4.dp), color = AppColors.mDarkPurple) {
-        Column(modifier = Modifier
-            .padding(12.dp), verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start) {
+fun QuestionDisplay(question: QuestionItem,
+                    QuestionIndex: MutableState<Int>,
+                    viewModel:QuestionsViewModel,
+                    onNextClicked: (Int) -> Unit) {
+
+    val choicesState = remember(question){
+        question.choices.toMutableList()
+    }
+
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp), color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp), verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
             QuestionTracker()
             DrawDottedLine(pathEffect)
+            Column {
+                Text(
+                    text = "What's the meaning of life", fontSize = 17.sp,
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(.3f),
+                    fontWeight = FontWeight.Bold, lineHeight = 22.sp, color = AppColors.mOffWhite
+                )
+                //Choices
+                choicesState.forEachIndexed { index, s ->
+                    Row(modifier = Modifier.padding(3.dp)
+                        .fillMaxWidth()
+                        .height(45.dp)
+                        .border(width = 4.dp,
+                                brush = Brush.linearGradient(
+                                colors = listOf(AppColors.mOffDarkPurple,AppColors.mDarkPurple)),
+                                shape = RoundedCornerShape(15.dp)).clip(RoundedCornerShape(topStartPercent = 50,
+                                topEndPercent = 50, bottomEndPercent = 50, bottomStartPercent = 50))
+                        .background(Color.Transparent)) {
+
+                    }
+                }
+            }
         }
 
     }
@@ -61,14 +109,17 @@ fun QuestionDisplay(){
 
 @Composable
 fun DrawDottedLine(pathEffect: PathEffect) {
-    Canvas(modifier = Modifier
-        .fillMaxWidth()
-        .height(1.dp),
-    ){
-        drawLine(color = AppColors.mLightGray,
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp),
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
             start = Offset(0f, 0f),
             end = Offset(size.width, y = 0f),
-            pathEffect = pathEffect)
+            pathEffect = pathEffect
+        )
 
     }
 
@@ -76,13 +127,23 @@ fun DrawDottedLine(pathEffect: PathEffect) {
 
 @Preview
 @Composable
-fun QuestionTracker(counter: Int = 10, outOf: Int = 100){
+fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
     Text(text = buildAnnotatedString {
-        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)){
-            withStyle(style = SpanStyle(color = AppColors.mLightGray,
-                fontWeight = FontWeight.Bold, fontSize = 27.sp)){
+        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+            withStyle(
+                style = SpanStyle(
+                    color = AppColors.mLightGray,
+                    fontWeight = FontWeight.Bold, fontSize = 27.sp
+                )
+            ) {
                 append("Question $counter/")
-                withStyle(style= SpanStyle(color = AppColors.mLightGray, fontWeight =  FontWeight.Light, fontSize = 14.sp)){
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 14.sp
+                    )
+                ) {
                     append("$outOf")
                 }
             }

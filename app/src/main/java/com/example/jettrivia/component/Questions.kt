c7523158e15b013e40c5b543d1f135f1ae857c29
Ulises Cadena <ucadena07@gmail.com>
2023-11-23 06:00:20 -0600

    creating a score meter

--- app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
@@ -196,7 +198,7 @@ fun DrawDottedLine(pathEffect: PathEffect) {
 
 }
 
-@Preview
+
 @Composable
 fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
     Text(text = buildAnnotatedString {
@@ -221,3 +223,48 @@ fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
         }
     }, modifier = Modifier.padding(2.dp))
 }
+
+@Preview
+@Composable
+fun ShowProgress(score: Int = 12){
+    val gradient = Brush.linearGradient(listOf(Color(0xFFF95075),Color(0xFFBE6BE5)))
+    Row(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(45.dp)
+        .border(
+            width = 4.dp,
+            brush = Brush.linearGradient(
+                colors = listOf(
+                    AppColors.mLightBlue,
+                    AppColors.mLightPurple
+                )
+            ),
+            shape = RoundedCornerShape(34.dp)
+        )
+        .clip(
+            RoundedCornerShape(
+                topEndPercent = 50,
+                topStartPercent = 50,
+                bottomStartPercent = 50,
+                bottomEndPercent = 50
+            )
+        )
+        .background(
+            Color.Transparent
+        ),
+        verticalAlignment = Alignment.CenterVertically) {
+        Button(onClick = { /*TODO*/ },
+            contentPadding = PaddingValues(1.dp),
+            modifier = Modifier.fillMaxWidth()
+                .background(brush = gradient), enabled = false,
+            elevation = null, colors = buttonColors(
+                containerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent
+            )) {
+
+        }
+    }
+}
+
+
